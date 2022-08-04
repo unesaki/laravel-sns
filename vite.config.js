9df39839f3f1910272e5e265711f285ca3cc3211
unesaki <97210193+unesaki@users.noreply.github.com>
2022-08-04 12:21:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import 'vite/modulepreload-polyfill'
 
 export default defineConfig({
     plugins: [
@@ -25,4 +26,10 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
+    build: {
+        minifest: true,
+        rollupOptions: {
+            input: '/path/to/main.js'
+        }
+    }
 });
